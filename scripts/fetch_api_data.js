5f97d1be05a68707c9d8db232a2e6de012e59692
Gabriele <savastrecosmingabriele@gmail.com>
2020-03-20 19:28:39 +0100

    Added different types of fetching

--- scripts/fetch_api_data.js
+++ scripts/fetch_api_data.js
@@ -1,9 +1,44 @@
-const url = "https://coronavirus-tracker-api.herokuapp.com/v2/latest";
+var url = "https://coronavirus-tracker-api.herokuapp.com/v2/";
 
+//Get latest cases : confirmed, deaths, recovered
 async function getLatest(){
-    const response = await fetch(url);
+    const param = "latest";
+    const response = await fetch(url.concat(param));
     const data = await response.json();
     console.log(data);
+    //return data;
+}
+//Get all locations : id, country, country_code, coords{}, latest{} without timelines
+async function getLocations(){
+    const param = "locations?timelines=0";
+    const response = await fetch(url.concat(param));
+    const data = await response.json();
+    console.log(data);
+    //return data;
 }
 
-getLatest();
+//TODO: Get multiple locations filtered from a list of IDs
+
+//Get single location from id
+async function getLocation(id){
+    const param = "locations/"+id;
+    const response = await fetch(url.concat(param));
+    const data = await response.json();
+    console.log(data);
+    //return data;
+}
+
+//Get all locations with timelines
+async function getTimelines(){
+    const param = "locations?timelines=1";
+    const response = await fetch(url.concat(param));
+    const data = await response.json();
+    console.log(data);
+    //return data;
+}
+
+/*getLatest().catch(error =>{
+    console.log(error);
+});
+
+getLocations();*/
