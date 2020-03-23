9ccde7becf16fde04c1a52e2dbec148dc9eec905
Gabriele <savastrecosmingabriele@gmail.com>
2020-03-23 15:46:34 +0100

    Added timelines to JSON result + location sorting by conf cases

--- js/fetch_api_data.js
+++ js/fetch_api_data.js
@@ -6,9 +6,17 @@ var overallData = {};
 
 //TODO: Load charts in another js file
 
+//First chart (Pie chart with global stats or stats of selected country)
 var fctx = document.getElementById('firstChart').getContext('2d');
+
+//Second chart(Horizontal bar chart with top 5 infected countries)
+var sctx = document.getElementById('secondChart').getContext('2d');
+
+//Third chart(Line chart with timeline of most infected country or selected country)
+var tctx = document.getElementById('thirdChart').getContext('2d');
+
 var labels = ['Confermati', 'Decessi', 'Guariti'];
-var colors = ['Red', 'Black', 'Green'];
+var colors = ['Red', 'Grey', 'Green'];
 var firstGraphData = [];
 
 //API base url
@@ -18,15 +26,17 @@ $(document).ready(function () {
     $.getJSON(url.concat("latest"), function(data){
 
         overallData = data.latest;
-        console.log(overallData);
+        //console.log(overallData);
 
         $('#infetti').text(overallData.confirmed);
         $('#deceduti').text(overallData.deaths);
         $('#guariti').text(overallData.recovered);
+
         $.each(overallData, function (key, value) {
             firstGraphData.push(parseInt(value));
         });
 
+        //Init first graph
         var firstChart = new Chart(fctx,{
            type: 'pie',
            data:{
@@ -52,7 +62,11 @@ $(document).ready(function () {
 
 
     //Get location based data
-    $.getJSON(url.concat("locations"), function(data){
-        locations = data.locations;
+    $.getJSON(url.concat("locations?timelines=1"), function(data){
+        locations = data.locations.sort(function (a, b) {
+            //Order descending
+            return (parseInt(a.latest.confirmed) - parseInt(b.latest.confirmed)) * -1;
+        });
+        console.log(locations);
     });
 });
