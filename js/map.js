d250fe7bdc553162dae59fc96c946022b2994f88
Cosmin <cosmindaxgugoasa@gmail.com>
2020-03-26 15:36:07 +0100

    Changed main counter size //popover NOT working

--- js/map.js
+++ js/map.js
@@ -1,18 +1,26 @@
 $(document).ready( function() {
+
     //hover functionalities
     $( "path" ).hover( function(){
         //get country alpha2 code
         var country_hovered = $(this).attr('id');
         var infected, deaths, recovered;
+
+
         //search for complete country name
         locations.forEach( function (location) {
-            if( country_hovered == location.country_code){
+            if( country_hovered === location.country_code){
                 country_hovered = location.country;
                 infected = location.latest.confirmed;
                 deaths = location.latest.deaths;
                 recovered = location.latest.recovered;
+            }else{
+                infected = "No data available.";
+                deaths = "No data available.";
+                recovered = "No data available.";
             }
         });
+
         //open popover with complete country name
         $(this).popover({
             html: true,
@@ -29,9 +37,10 @@ $(document).ready( function() {
         //get clicked country alpha2 code
         var country_clicked = $(this).attr('id');
         console.log(country_clicked);
+
         //search for clicked country data
         locations.forEach( function (location) {
-            if( country_clicked == location.country_code){
+            if( country_clicked === location.country_code){
                 console.log(location.latest);
                 $('#target_coutry').text(location.country);
                 $('#infetti').text(location.latest.confirmed);
