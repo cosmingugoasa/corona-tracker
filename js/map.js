dffdf5c6650f78bb1a331d4fe5dcd5056149d090
Gabriele <savastrecosmingabriele@gmail.com>
2020-03-28 12:04:37 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready( function() {
     //hover functionalities
     $( "path" ).hover( function(){
@@ -33,3 +34,52 @@ $(document).ready( function() {
         });
     });
 });
+=======
+$(document).ready( function() {
+
+    //enable pan&zoom
+    var panZoomTiger = svgPanZoom('#svg');
+
+    //hover functionalities
+    $( "path" ).hover( function(){
+        //get country alpha2 code
+        var country_hovered = $(this).attr('id');
+        var infected, deaths, recovered;
+
+
+        //search for complete country name
+        locations.forEach( function (location) {
+            if( country_hovered === location.country_code){
+                country_hovered = location.country;
+                infected = location.latest.confirmed;
+                deaths = location.latest.deaths;
+                recovered = location.latest.recovered;
+            }
+        });
+
+        //open popover with complete country name
+        $(this).popover({
+            title: country_hovered,
+            content: "Click to see the counter for this country.",
+            container: 'body'
+        });
+    });
+
+    //click functionalities
+    $( "path" ).click( function () {
+        //get clicked country alpha2 code
+        var country_clicked = $(this).attr('id');
+
+        //search for clicked country data
+        locations.forEach( function (location) {
+            if( country_clicked === location.country_code){
+                console.log(location.latest);
+                $('#target_coutry').text(location.country);
+                $('#infetti').text(location.latest.confirmed);
+                $('#deceduti').text(location.latest.deaths);
+                $('#guariti').text(location.latest.recovered);
+            }
+        });
+    });
+});
+>>>>>>> develop
