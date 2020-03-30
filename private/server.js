ded9d4f13906c209e8790afd01d02780887eaac3
Cosmin <cosmindaxgugoasa@gmail.com>
2020-03-30 15:36:37 +0200

    Server init + basic comunication

---
+++ private/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var path = require('path');
+
+app.use('/', express.static(path.join(__dirname, '../')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+    console.log("received get / request");
+});
+
+server.listen(7777);
+console.log("server listening . . .");
