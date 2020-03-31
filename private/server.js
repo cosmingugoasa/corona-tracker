a41e6e825f9fb6f817fb1481c70d4e23fdcb5217
Cosmin <cosmindaxgugoasa@gmail.com>
2020-03-31 15:40:14 +0200

    Added MongoDB comunication

--- private/server.js
+++ private/server.js
@@ -3,6 +3,11 @@ var app = express();
 var server = require('http').Server(app);
 var path = require('path');
 var bp = require('body-parser');
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    email: 'string'
+});
 
 app.use(bp());
 app.use('/', express.static(path.join(__dirname, '../')));
@@ -12,8 +17,25 @@ app.get('/', function (req, res) {
     console.log("received get / request");
 });
 
-app.post('/sub', function (req, res) {
+app.post('/sub', async function (req, res) {
     console.log(JSON.stringify(req.body.emailinput));
+
+    mongoose.connect('mongodb://cosmingugoasa:GC15973k@mongodb-cosmingugoasa.alwaysdata.net/cosmingugoasa_coronatracker', {useNewUrlParser: true, useUnifiedTopology: true });
+    var db = mongoose.connection;
+    db.on('error', function (error) {
+        console.log(error);
+    });
+    db.once('open', function () {
+        console.log("Connected to Database :");
+    });
+
+    var users = mongoose.model('Users', userSchema);
+    var new_user = new users({email: req.body.emailinput});
+    await new_user.save(function (err) {
+        if (err) console.log(err);
+        else console.log("Added : " + req.body.emailinput);
+    });
+
     res.redirect('/');
 });
 
