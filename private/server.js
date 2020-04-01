3c7436eb1015e838a792756ef50d27ff6a0f9aec
Cosmin <cosmindaxgugoasa@gmail.com>
2020-04-01 13:27:49 +0200

    Added email retrieve on server startup

--- private/server.js
+++ private/server.js
@@ -5,9 +5,32 @@ var path = require('path');
 var bp = require('body-parser');
 var mongoose = require('mongoose');
 
+require('dotenv').config();
+
 var userSchema = mongoose.Schema({
     email: 'string'
 });
+var users = mongoose.model('Users', userSchema);
+
+//database connection
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', function (error) {
+    console.log(error);
+});
+db.once('open', function () {
+    console.log("Connected to Database. \n");
+});
+
+users.find({}, {_id: 0, __v: 0}, function (err, data) {
+    console.log("Subscribed users :");
+    data.forEach(function (element) {
+        console.log(element.email);
+        //TODO SEND MAIL TO element.mail
+    })
+});
+
+
 
 app.use(bp());
 app.use('/', express.static(path.join(__dirname, '../')));
@@ -18,18 +41,6 @@ app.get('/', function (req, res) {
 });
 
 app.post('/sub', async function (req, res) {
-    console.log(JSON.stringify(req.body.emailinput));
-
-    mongoose.connect('mongodb://cosmingugoasa:GC15973k@mongodb-cosmingugoasa.alwaysdata.net/cosmingugoasa_coronatracker', {useNewUrlParser: true, useUnifiedTopology: true });
-    var db = mongoose.connection;
-    db.on('error', function (error) {
-        console.log(error);
-    });
-    db.once('open', function () {
-        console.log("Connected to Database :");
-    });
-
-    var users = mongoose.model('Users', userSchema);
     var new_user = new users({email: req.body.emailinput});
     await new_user.save(function (err) {
         if (err) console.log(err);
@@ -40,4 +51,4 @@ app.post('/sub', async function (req, res) {
 });
 
 server.listen(7777);
-console.log("server listening . . .");
+console.log("\nServer listening . . .");
